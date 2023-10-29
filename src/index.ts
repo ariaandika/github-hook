c8eb4148593fa545bd63ff53299b271064d5ca71
ariaandika <andikaakun17@gmail.com>
2023-10-29 09:53:52 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+
+
+Bun.serve({
+  fetch(req) {
+    const url = new URL(req.url)
+
+    if (url.pathname === '/github') {
+      return githubHook(req, url)
+    }
+
+
+    return new Response('Hello World')
+  },
+})
+
+// route matching
+async function githubHook(req: Request, u: URL) {
+
+  if (req.method !== 'POST') {
+    return new Response('', { status: 405 })
+  }
+
+  const sig = req.headers.get('X-Hub-Signature')
+  const event = req.headers.get('X-GitHub-Event')
+  const id = req.headers.get('X-GitHub-Delivery')
+  const isJson = req.headers.get('content-type') == 'application/json'
+
+  if (!sig || !event || !id || !isJson) {
+    return new Response('', { status: 401 })
+  }
+
+  try {
+    const body = await req.text()
+    const payload = JSON.parse(body)
+
+    const repo = payload.repository.full_name
+
+    console.log(payload)
+
+    if (repo != 'ariaandika/dev-serve') {
+      throw undefined
+    }
+
+    return new Response('', { status: 400 })
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error)
+    }
+    return new Response('', { status: 400 })
+  }
+}
+
